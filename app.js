e79a15c26d46ca6c64b43d486d2ec9ae79cca764
mabirizi <mabiriziad@gmail.com>
2019-10-14 17:18:09 +0300

    mapbox and darksky

---
+++ app.js
@@ -0,0 +1,12 @@
+//darksky api
+//https://api.darksky.net/forecast/b416497d8e487fa7303ea56fe61d810a/37.8267,-122.4233
+
+//b416497d8e487fa7303ea56fe61d810a
+
+
+
+// map box acces token
+// pk.eyJ1IjoibWFiaXJpemkiLCJhIjoiY2sxcWhxbzV1MDkzbjNkbzFrNDAwaWRmMyJ9.Nrm5-yOIWECe-nTNPJ865Q
+
+
+// https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWFiaXJpemkiLCJhIjoiY2sxcWhxbzV1MDkzbjNkbzFrNDAwaWRmMyJ9.Nrm5-yOIWECe-nTNPJ865Q
